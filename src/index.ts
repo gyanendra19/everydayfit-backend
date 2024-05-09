5fd6ab1da6bea85860994b21fe2456bbd00dbb48
gyanendra19 <gyanendrav416@gmail.com>
2024-05-09 17:17:02 +0530

    added header

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ mongoose.connect(DB).then(() => {
 })
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Replace * with specific origins if needed
+    res.header('Access-Control-Allow-Origin', 'https://everydayfit.vercel.app/'); // Replace * with specific origins if needed
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
